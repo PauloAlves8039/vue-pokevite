efd4aa38ce663037ad214134be8c7422a2cd9f8c
PauloAlves8039 <pj38alves@gmail.com>
2023-03-18 11:39:51 -0300

    Criando o template de aplicação

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,41 @@
 </script>
 
 <template>
-  <h1>Pokevite</h1>  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">
+          <img src="https://cdn-icons-png.flaticon.com/512/188/188987.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+          PokeVite
+        </router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <footer class="bg-primary text-light">
+      <div>Design in {{ new Date().getFullYear() }}</div>
+    </footer>
 </template>
 
-<style scoped>
-
+<style>
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
+}
 </style>
