39e684b74a58c26d4cc425ac020d177d315795d6
PauloAlves8039 <pj38alves@gmail.com>
2023-03-19 15:53:03 -0300

    Criando conexão com a API e exibindo dados

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,41 @@
 <script setup>
+import { onMounted, reactive } from 'vue';
+
+let pokemons = reactive();
+
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+    .then(res => res.json())
+    .then(res => pokemons = res.results);
+});
 
 </script>
 
 <template>
   <main>
-    Home
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="https://t2.tudocdn.net/301383?w=1920" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <ul>
+              <li v-for="pokemon in pokemons" :key="pokemon.name">
+                {{ pokemon.name }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
