3e888f4afa1d9862192be63459ea1927ffa4e05b
PauloAlves8039 <pj38alves@gmail.com>
2023-03-28 16:03:55 -0300

    Implementando Bootstrap e Animate CSS

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'animate.css';
+
 const app = createApp(App)
 
 app.use(router)
